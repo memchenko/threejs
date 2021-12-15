9e14114ab4605913b6b4c308ae0ab2534786b334
memchenko <emchenko.mikhail@mail.ru>
2021-12-16 02:24:39 +0400

    add shader patterns

--- src/script.js
+++ src/script.js
@@ -23,4 +23,5 @@ import "./style.css";
 // import "./24-custom-models-with-blender";
 // import "./25-realistic-render";
 // import "./26-code-structuring-for-bigger-projects";
-import "./27-shaders";
+// import "./27-shaders";
+import "./28-shader-patterns";
